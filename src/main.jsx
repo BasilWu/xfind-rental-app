daee1eb04972347907d241034bf1054c11a627bc
Basil Wu <xxxtueur@gmail.com>
2025-05-22 18:03:21 +0800

    chore: initial project setup with Vite, React, Tailwind

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import './index.css'    // ← 一定要在這裡 import
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
